c211043fbd07da926c51bee5729ab9d68b8909b9
joao amaro <joaovictoramaroalves@gmail.com>
2025-12-26 17:47:51 -0300

    fix/ refatoração de algumas partes lógicas das exception, reaproveitamento de funções dentro do GlobalExceptionHandler como o buildError. e alteração na dto ResponseError

--- src/main/java/com/SriLankaCard/dto/response/exceptionHandler/ResponseError.java
+++ src/main/java/com/SriLankaCard/dto/response/exceptionHandler/ResponseError.java
@@ -1,44 +1,17 @@
 package com.SriLankaCard.dto.response.exceptionHandler;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.time.LocalDateTime;
+import java.time.Instant;
 
     @Data
+    @AllArgsConstructor
     public class ResponseError {
         private int status;
         private String message;
-        private LocalDateTime timestamp;
+        private Instant timestamp;
+        private String path;
 
-        public ResponseError(int status, String message, LocalDateTime timestamp) {
-            this.status = status;
-            this.message = message;
-            this.timestamp = timestamp;
-        }
-
-        public int getStatus() {
-            return status;
-        }
-
-        public void setStatus(int status) {
-            this.status = status;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public LocalDateTime getTimestamp() {
-            return timestamp;
-        }
-
-        public void setTimestamp(LocalDateTime timestamp) {
-            this.timestamp = timestamp;
-        }
     }
 
 
