dc1029935b619abdfb0ea9983de51d6651a39ce9
joao amaro <joaovictoramaroalves@gmail.com>
2026-01-13 08:34:57 -0300

    feat/ Criação dos DTOs de response para pedido

---
+++ src/main/java/com/SriLankaCard/dto/response/pedido/PedidoHistoricoResponse.java
@@ -0,0 +1,20 @@
+package com.SriLankaCard.dto.response.pedido;
+
+import com.SriLankaCard.entity.pedidoEntity.ItemPedido;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PedidoHistoricoResponse {
+    private Long id;
+    private Double valorTotal;
+    private Integer quantidadeTotal;
+    private LocalDateTime criadoEm;
+    private List<ItemPedidoResponse> itensPedido;
+}
